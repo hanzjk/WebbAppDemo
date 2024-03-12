0219652644d6291bc70dd93a8329ec4f691b7633
hanzjk <hansijk@yahoo.com>
2024-03-12 21:23:09 +0530

    Update code

--- frontend-app/src/api.ts
+++ frontend-app/src/api.ts
@@ -4,12 +4,14 @@ import { performRequestWithRetry } from "./retry";
 export async function getBooks() {
 
 const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
-
+console.log(apiUrl);
  const options = {
     method: 'GET',
   };
 
   const response = await performRequestWithRetry(apiUrl, options);
+  console.log(response);
+
   return response;
 }
 
