7456eed3251c54e649a819b978af4f4d887b5196
hanzjk <hansijk@yahoo.com>
2024-03-16 18:19:10 +0530

    Choreo-ouh

--- frontend-app-js/public/config.js
+++ frontend-app-js/public/config.js
@@ -1,3 +1,3 @@
 window.configs = {
-  apiUrl: 'choreo-apis/zkyf/hello-world/hello-88e/v1.0/greeting',
+  apiUrl: 'https://114531ee-70da-468b-aafc-00dc439baac2-dev.e1-us-east-azure.choreoapis.dev/atze/backendservice/hello-88e/v1.0',
 };
--- frontend-app-js/src/App.js
+++ frontend-app-js/src/App.js
@@ -8,32 +8,32 @@ function App() {
 
   const [user, setUser] = useState(null);
 
-  // useEffect(() => {
-  //   if (Cookies.get('userinfo')) {
-  //     // We are here after a login
-  //     const userInfoCookie = Cookies.get('userinfo')
-  //     sessionStorage.setItem("userInfo", userInfoCookie);
-  //     Cookies.remove('userinfo');
-  //     var userInfo = JSON.parse(atob(userInfoCookie));
-  //     setSignedIn(true);
-  //     setUser(userInfo);
-  //   } else if (sessionStorage.getItem("userInfo")) {
-  //     // We have already logged in
-  //     var userInfo = JSON.parse(atob(sessionStorage.getItem("userInfo")));
-  //     setSignedIn(true);
-  //     setUser(userInfo);
-  //   } else {
-  //     console.log("User is not signed in");
-  //   }
-  // }, []);
+  useEffect(() => {
+    if (Cookies.get('userinfo')) {
+      // We are here after a login
+      const userInfoCookie = Cookies.get('userinfo')
+      sessionStorage.setItem("userInfo", userInfoCookie);
+      Cookies.remove('userinfo');
+      var userInfo = JSON.parse(atob(userInfoCookie));
+      setSignedIn(true);
+      setUser(userInfo);
+    } else if (sessionStorage.getItem("userInfo")) {
+      // We have already logged in
+      var userInfo = JSON.parse(atob(sessionStorage.getItem("userInfo")));
+      setSignedIn(true);
+      setUser(userInfo);
+    } else {
+      console.log("User is not signed in");
+    }
+  }, []);
 
 
   useEffect(() => {
     getReadingList();
-  }, []);
+  }, [signedIn]);
 
   async function getReadingList() {
-   if (true){
+   if (signedIn){
     getBooks()
     .then((res) => {
       setData(res.data);
@@ -46,20 +46,19 @@ function App() {
     
   }
 
-  // if (!signedIn) {
-  //   return (
-  //     <>
-  //     <h1>{test}</h1>
-  //      <button
-  //       className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
-  //       onClick={() => { window.location.href = "/auth/login" }}
-  //     >
-  //       Login
-  //     </button>
-  //     </>
+  if (!signedIn) {
+    return (
+      <>
+       <button
+        className="float-right bg-black bg-opacity-20 p-2 rounded-md text-sm my-3 font-medium text-white"
+        onClick={() => { window.location.href = "/auth/login" }}
+      >
+        Login
+      </button>
+      </>
      
-  //   );
-  // }
+    );
+  }
 
 
   return (
@@ -69,7 +68,7 @@ function App() {
         <p>Received Response</p>
         <p>{data}</p>
        
-        {/* <button
+        <button
               className="float-right bg-[#5b86e5] p-2 rounded-md text-sm my-3 font-medium text-white"
               onClick={() => {
                 sessionStorage.removeItem("userInfo");
@@ -77,7 +76,7 @@ function App() {
               }}
             >
               Logout
-            </button> */}
+            </button>
       </body>
     </div>
   );
