942b07b05e5d12f696118978d4dd5add1cbfe0b7
hanzjk <hansijk@yahoo.com>
2024-03-17 23:43:18 +0530

    Add authentication

--- my-app-next-js/src/pages/greetings.js
+++ my-app-next-js/src/pages/greetings.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import oauth from 'axios-oauth-client'
+import axios from 'axios';
 
 function Greetings({ posts }) {
   return (
@@ -14,8 +16,22 @@ function Greetings({ posts }) {
 // This function gets called at request time
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(process.env.API_URL+'/greeting')
-  const posts = await res.text()
+  const getClientCredentials = oauth.clientCredentials(
+    axios.create(),
+    process.env.TOKEN_URL,
+    process.env.CONSUMER_KEY,
+    process.env.CONSUMER_SECRET
+  );
+  const auth = await getClientCredentials();
+  const accessToken = auth.access_token;
+
+  const response = await axios.get(process.env.API_URL+'/greeting', {
+    headers: {
+      'Authorization': `Bearer ${accessToken}`
+    }
+});
+
+  const posts = await response.data
 
   // Pass data to the page via props
   return { props: { posts } }
