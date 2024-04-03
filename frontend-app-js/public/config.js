17563d49d8543058474d79034d30a88c110063e7
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-04-03 18:37:01 +0530

    Update config.js

--- frontend-app-js/public/config.js
+++ frontend-app-js/public/config.js
@@ -1,3 +1,3 @@
 window.configs = {
-  apiUrl: 'https://114531ee-70da-468b-aafc-00dc439baac2-dev.e1-us-east-azure.choreoapis.dev/atze/backendservice/hello-88e/v1.0',
+    apiUrl: '/choreo-apis/atze/backendservice/hello-88e/v1.0',
 };
