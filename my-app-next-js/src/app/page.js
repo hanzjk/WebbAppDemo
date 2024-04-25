94cd91f619fda8bad18c37020d8bb10dcf8bc993
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-04-25 20:26:20 +0530

    Update page.js

--- my-app-next-js/src/app/page.js
+++ my-app-next-js/src/app/page.js
@@ -5,6 +5,8 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+          <h1>{process.env.API_URL}</h1>
+
       <Link href="/greetings">Greetings</Link>
 
       </div>
