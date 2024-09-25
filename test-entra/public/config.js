71f2d2d4ef79d69471f9881b04c31d0d3dc272bc
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-09-26 00:58:45 +0530

    Update config.js

--- test-entra/public/config.js
+++ test-entra/public/config.js
@@ -1,6 +1,6 @@
 window.configs = {
-  apiUrl: '/choreo-apis/atze/backendservice/hello-88e/v1.0',
-  appId:'a7098e37-0883-44ec-a934-71c6cfe9fd5a',
+  apiUrl: '/choreo-apis/appointment-management/testservicenew/v1/greeting',
+  appId:'c95c2df6-4053-4e2f-8432-d3bcbab10f64',
   redirectUri:'http://localhost:3000',
   authority:'https://login.microsoftonline.com/da76d684-740f-4d94-8717-9d5fb21dd1f9',
   scopes:['user.read']
