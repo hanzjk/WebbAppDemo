986114573e376ee724a9df1a115a782c733bafb0
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-09-26 01:00:31 +0530

    Update App.js

--- test-entra/src/App.js
+++ test-entra/src/App.js
@@ -58,7 +58,7 @@ function App() {
   };
 
   const callAPI = (token) => {
-    const apiURL = 'https://114531ee-70da-468b-aafc-00dc439baac2-dev.e1-us-east-azure.choreoapis.dev/atze/backendservice/hello-88e/v1.0/greeting';
+    const apiURL = window.configs.apiUrl;
 
     fetch(apiURL, {
       method: 'GET',
