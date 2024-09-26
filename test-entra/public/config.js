06aaa55a33a6443c17d85948e88e943ea8b8a934
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2024-09-26 10:17:45 +0530

    Update config.js

--- test-entra/public/config.js
+++ test-entra/public/config.js
@@ -1,7 +1,7 @@
 window.configs = {
   apiUrl: '/choreo-apis/appointment-management/testservicenew/v1/greeting',
   appId:'c95c2df6-4053-4e2f-8432-d3bcbab10f64',
-  redirectUri:'https://d4fd81d5-103d-4a58-9a42-9eff9b3c3762.e1-us-east-azure.choreoapps.dev',
+  redirectUri:'https://d4fd81d5-103d-4a58-9a42-9eff9b3c3762.e1-us-east-azure.choreoapps.dev/auth/login/callback',
   authority:'https://login.microsoftonline.com/da76d684-740f-4d94-8717-9d5fb21dd1f9',
   scopes:['user.read']
 };
