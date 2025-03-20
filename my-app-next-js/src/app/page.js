d9eea6fc09ed600d553e17753f33adca8f1397a5
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2025-03-20 12:28:09 +0530

    Update page.js

--- my-app-next-js/src/app/page.js
+++ my-app-next-js/src/app/page.js
@@ -1,18 +1,12 @@
+import Greetings from "@/pages/greetings";
 import styles from "./page.module.css";
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-              <h1>Env varibales</h1>
-
-          <h1>{process.env.API_URL}</h1>
-          <h1>{process.env.CONSUMER_KEY}</h1>
-          <h1>{process.env.CONSUMER_SECRET}</h1>
-          <h1>{process.env.TOKEN_URL}</h1>
-      <Link href="/greetings">Greetings</Link>
-
+        <Link href="/greetings">Greetings</Link>
       </div>
     </main>
   );
