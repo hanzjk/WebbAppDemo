c8743f5adf937b601645ddb4b7438a99d9fa5650
Hansi Karunarathna <65526190+hanzjk@users.noreply.github.com>
2025-03-20 12:09:01 +0530

    Update page.js

--- my-app-next-js/src/app/page.js
+++ my-app-next-js/src/app/page.js
@@ -6,7 +6,9 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
           <h1>{process.env.API_URL}</h1>
-
+          <h1>{process.env.CONSUMER_KEY}</h1>
+          <h1>{process.env.CONSUMER_SECRET}</h1>
+          <h1>{process.env.TOKEN_URL}</h1>
       <Link href="/greetings">Greetings</Link>
 
       </div>
